4bdae1657199105b87b1adf1eca01087be0cf8f8
summer <D__yanan@163.com>
2018-05-20 13:18:31 +0800

    Add listener for input box's keyup

--- src/jquery.autocomplete.js
+++ src/jquery.autocomplete.js
@@ -1,5 +1,21 @@
 (function ($) {
     $.fn.autocompleteToken = function (keycode, sourceData) {
-
+        var ulNode=document.createElement('ul');
+        $(ulNode).css({"display":"none"});
+        if(sourceData){
+            sourceData.forEach(function(data){
+                if(data){
+                    var liNode=document.createElement('li');
+                    liNode.innerHTML=data;
+                    ulNode.appendChild(liNode);
+                }
+            });
+        }
+        this.keyup(function(e){
+            if(e.keyCode === keycode){
+                document.body.appendChild(ulNode);
+                $(ulNode).css({"display":"inline"});
+            }
+        })
     };
 }(jQuery));
