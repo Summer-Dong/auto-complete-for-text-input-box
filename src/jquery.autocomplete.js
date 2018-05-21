445f4cd4441448d09e4a8050fc717d1ea200c972
Summer <D__yanan@163.com>
2018-05-21 20:53:27 +0800

    [#5]Get/set caret's position.

--- src/jquery.autocomplete.js
+++ src/jquery.autocomplete.js
@@ -1,4 +1,33 @@
 (function ($) {
+    function getCaretPosition(domElement) {
+        var iCaretPos = 0;
+        // IE Support
+        if (document.selection) {
+            domElement.focus();
+            var oSel = document.selection.createRange();
+            oSel.moveStart('character', -domElement.value.length);
+            iCaretPos = oSel.text.length;
+        }
+        // Firefox support
+        else if (domElement.selectionStart || domElement.selectionStart === '0') {
+            iCaretPos = domElement.selectionStart;
+        }
+        return iCaretPos;
+    }
+
+    function setCaretPosition(domElement, pos) {
+        if (domElement.setSelectionRange) {
+            domElement.focus();
+            domElement.setSelectionRange(pos, pos);
+        } else if (domElement.createTextRange) {
+            var range = domElement.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', pos);
+            range.moveStart('character', pos);
+            range.select();
+        }
+    }
+
     $.fn.autocompleteToken = function (keycode, sourceData) {
         var ulNode=document.createElement('ul');
         $(ulNode).css({"display":"none"});
